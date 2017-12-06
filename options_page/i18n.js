d11f518e544ba11313058d4dcf8aac755410fbda
SUNVIEW <32350723+hakunacode@users.noreply.github.com>
2017-12-07 07:37:45 +0800

    Add files via upload

---
+++ options_page/i18n.js
@@ -0,0 +1,158 @@
+// SAMPLE
+this.i18n = {
+    "settings": {
+        "en": "Options",
+        "de": "Optionen"
+    },
+    "search": {
+        "en": "Search",
+        "de": "Suche"
+    },
+    "nothing-found": {
+        "en": "No matches were found.",
+        "de": "Keine Übereinstimmungen gefunden."
+    },
+    
+    "default-theme":{
+        "en": "Default Theme (Dark)",
+
+    },
+    "white-theme":{
+        "en": "Modern Light (White)",
+
+    },
+    "old-theme":{
+        "en":"Retro Theme (Dark)",
+
+    },
+    
+    "themes": {
+        "en": "Calculator Theme",
+
+    },
+    "theme-preview": {
+        "en": "Theme Preview",
+        "de": "Information"
+    },
+
+    "description": {
+        "en": "Select your preferred theme from the group above. Here are the three currently supported themes:\
+        <br /><br />Default Theme (Dark) -- this is the new dark theme, in somehow modern style:<a id='def1' href='#'><img src='default.png' width='500px' /></a><br>\
+        <br />White Theme (Light) -- a white design, if you are not into the dark style of the default theme:<a id='white1' href='#'><img src='white.png' width='500px' style='border:solid 1px lightgrey'  /></a><br>\
+        <br />Retro Theme (Dark) -- this is the original theme, in case you miss it:<a id='old1' href='#'><img src='old.png' width='500px'  /></a><br>\
+        ",
+
+    },
+    "resetTheme": {
+        "en": "Reset Theme",
+
+    },
+    "enable": {
+        "en": "Enable",
+        "de": "Aktivieren"
+    },
+    "resetThemesPage": {
+        "en": "Restore Default Theme:",
+
+    },
+    "currentTheme": {
+        "en": "Current Theme",
+
+    },  
+ 
+    "kbshortcut": {
+        "en": "Keyboard Shortcut",
+    },  
+    
+    "kbshortcut-label":{
+        "en": "Shortcut"
+    },    
+    "kbshortcut-2":{
+        "en": "How to..."
+    },
+   "opensettingslbl":{
+        "en": "Go to 'Extensions':"
+    },    
+   "opensettings":{
+        "en": "Open 'Extensions' Page"
+    },
+    "kbshortcut-descr":{
+        "en":   "By default the calculator will try to assign <strong>Alt+C</strong> as keyboard shortcut. \
+                If that doesn't work (sometimes Chrome refuses to accept the shortcut we add during install) \
+                and you still want the keyboard shortcut, you can add it manually (see below)."
+    },    
+    "kbshortcut-descr2":{
+        "en":   "If you want to change it (or the shortcut for any other extension) -- go to the \
+        '<b>chrome://extensions/</b>' page, scroll down to the end of the page and click the '<b>Keyboard shortcuts</b>' \
+        link at the bottom-right corner."
+    } ,
+    
+    "more": {
+        "en": "Info & More",
+    },      
+     "more-label":{
+        "en": "Rate Us"
+    }, 
+     "more-descr":{
+        "en":   "If you like this extension, please rate it:"
+    },  
+     "more-text":{
+        "en":   "Rate us in Chrome Web Store"
+    }, 
+
+     "about-label":{
+        "en":   "About"
+    },     
+     "about-descr":{
+        "en":   "<b>Melanto Calculator</b><br />\
+                Copyright (C) 2011-2017, Melanto Ltd., All Rights Reserved\
+                <br />Disclaimer of warranties:\
+                this software is provided “as is” and without warranty of any kind"
+    },  
+     "about-url":{
+        "en":   "www.melanto.com"
+    }, 
+     "about-visit":{
+        "en":   "For more info and other extensions, visit our site:"
+    }, 
+     "visit-label":{
+        "en": "Links"
+    },     
+    
+    
+    "ads-label":{
+        "en": "More Extensions"
+    }, 
+    
+    "ads":{
+        "en": "<p>Did you know that we have some other extensions? Here are some of them:</p> \
+    <div style=\"border: 1px solid #F1F0F3;background-color:#F8FFFF\">\
+    <table border=\"0\">\
+      <tr>\
+        <td width=\"135\"><a target='_blank' href=\"https://chrome.google.com/webstore/detail/bulk-url-opener-extension/hgenngnjgfkdggambccohomebieocekm\"><img src=\"bulk-128.png\" alt=\"screenshot\"></a></td>\
+        <td ><p><strong> <h4><a target='_blank' href='https://chrome.google.com/webstore/detail/bulk-url-opener-extension/hgenngnjgfkdggambccohomebieocekm'>Bulk URL Opener</a></h4>\
+          Open multiple URLs at once: Just enter each URL in new line (you can paste the list if you prefer) \
+            and click the \"Open All\" button. </strong></p>\
+         \
+          <p><a class=\"btn\" target='_blank' href=\"https://chrome.google.com/webstore/detail/bulk-url-opener-extension/hgenngnjgfkdggambccohomebieocekm\">\
+            <strong>Get it in Chrome Web Store</strong> &raquo;</a></p></td>\
+      </tr>\
+    </table>\
+    </div>\
+        <br />\
+<div style=\"border: 1px solid #F1F0F3;background-color:#F8FFFF\">\
+    <table border=\"0\">\
+      <tr>\
+        <td width=\"135\"><a target='_blank' href=\"https://chrome.google.com/webstore/detail/most-visited/obbnkbhoknnlndofpoikddaompgmiioc\"><img src=\"most-128.png\" alt=\"screenshot\"></a></td>\
+        <td ><p><strong> <h4><a target='_blank' href='https://chrome.google.com/webstore/detail/most-visited/obbnkbhoknnlndofpoikddaompgmiioc'>Most Visited</a></h4>\
+          Access your most visited pages (top sites) in a simple drop-down list. </strong></p>\
+         \
+          <p><a class=\"btn\" target='_blank' href=\"https://chrome.google.com/webstore/detail/most-visited/obbnkbhoknnlndofpoikddaompgmiioc\">\
+            <strong>Get it in Chrome Web Store</strong> &raquo;</a></p></td>\
+      </tr>\
+    </table>\
+    </div>"
+    },  
+    
+    
+};
